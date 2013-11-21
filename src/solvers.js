0edef1fa4b0032c847375d76c7ff6dea96d17887
Hack Reactor Students <cooperjbrandon@gmail.com>
2013-11-21 10:52:14 -0800

    refactored n queens

--- src/solvers.js
+++ src/solvers.js
@@ -100,28 +100,41 @@ window.findNQueensSolution = function(n){
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n){
+  var makeCopies = function (badColumn, badMaj, badMin, rowCounter) {
+    var copyObj = {};
+
+    copyObj.usedColumn = badColumn.slice(0);
+    copyObj.majorD = badMaj.slice(0);
+    copyObj.minorD = badMin.slice(0);
+    copyObj.rowCounter = rowCounter.slice(0);
+
+    return copyObj;
+  };
+
+  var pushData = function(copyObj, i) {
+    copyObj.usedColumn.push(i);
+    copyObj.majorD.push(i-copyObj.rowCounter[0]);
+    copyObj.minorD.push(i+copyObj.rowCounter[0]);
+  };
+
   var result = [];
   var notFun = function(n, col, badColumn, badMaj, badMin, rowCounter){
     for (var i = 0; i < col; i++){
       if (!_.contains(badColumn,i) && !_.contains(badMaj,i-rowCounter[0]) && !_.contains(badMin, i+rowCounter[0])){
-        var copy = badColumn.slice(0);
-        var copyMaj = badMaj.slice(0);
-        var copyMin = badMin.slice(0);
-        var copyCounter = rowCounter.slice(0);
-        copy.push(i);
-        copyMaj.push(i-copyCounter[0]);
-        copyMin.push(i+copyCounter[0]);
+        var copyObj = makeCopies(badColumn, badMaj, badMin, rowCounter);
+        pushData(copyObj, i);
         if (n !== 1) {
-          ++copyCounter[0];
-          notFun(n-1, col, copy, copyMaj, copyMin, copyCounter);
+          ++copyObj.rowCounter[0];
+          notFun(n-1, col, copyObj.usedColumn, copyObj.majorD, copyObj.minorD, copyObj.rowCounter);
         } else {
-          // debugger;
-          result.push(copy);
+          result.push(copyObj.usedColumn);
         }
       }
     }
   };
+
   notFun(n, n, [], [], [], [0]);
+
   // console.log('Number of solutions for ' + n + ' queens:',);
   return result.length;
 };
